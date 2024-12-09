0b4149ae8da046313229cf99f2d0281f418c1e65
Kelly Cho <chokelly@sas.upenn.edu>
2024-12-09 14:54:14 -0500

    Add files via upload

---
+++ portfolio/sketch.js
@@ -0,0 +1,44 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight * 0.8);
+
+    img1 = loadImage('flower6.webp'); // s1a1
+    img2 = loadImage('flower10.webp'); //s1a2
+    img3 = loadImage('flower5.webp'); //s1fp
+    img4 = loadImage('flower4.webp'); //s2a1
+    img5 = loadImage('flower9.webp'); // s2a2
+    img6 = loadImage('flower1.webp'); // s2fp
+    img7 = loadImage('flower7.webp'); // s3a1
+    img8 = loadImage('flower8.webp'); // s3a2
+    img9 = loadImage('flower2.webp'); // s3fp
+    img10 = loadImage('flower3.png'); // responses
+
+    xArr = [(width * 0.1), (width * 0.3), (width * 0.5), (width * 0.7), (width * 0.9)];
+    yArr = [(height * 0.1), (height * 0.3), (height * 0.5), (height * 0.7), (height * 0.9)];
+    imgArr = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
+    flowerArr = [];
+}
+
+function draw() {
+    background('blue');
+
+    for (let i = 0; i < 10; i++) {
+        xIndex = round(random(0, 9));
+        yIndex = round(random(0, 9));
+        imgIndex = round(random(0, 9));
+
+        flowerArr[i] = new Flower(xArr(xIndex), yArr(yIndex), imgArr(imgIndex));
+        flowerArr[i].display();
+    }
+}
+
+class Flower {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.img;
+    }
+
+    display() {
+        image(img, x, y, (width * 0.05), (width * 0.075));
+    }
+}
